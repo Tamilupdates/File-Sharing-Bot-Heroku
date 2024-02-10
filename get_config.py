11e2e6469d3154b3316b5c4e536a7d9fae548ce5
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2024-02-10 14:45:55 +0530

    Update get_config.py

--- get_config.py
+++ get_config.py
@@ -1,19 +1,20 @@
+import logging
 from requests import get as rget
 import os
 
+logging.basicConfig(level=logging.ERROR)
+LOGGER = logging.getLogger(__name__)
+
 CONFIG_FILE_URL = os.environ.get('CONFIG_FILE_URL')
 try:
-    if len(CONFIG_FILE_URL) == 0:
-        raise TypeError
-    try:
-        res = rget(CONFIG_FILE_URL)
-        if res.status_code == 200:
-            with open('config.py', 'wb+') as f:
-                f.write(res.content)
-        else:
-            LOGGER.error(f"Failed to download config.py {res.status_code}")
-    except Exception as e:
-        LOGGER.error(f"CONFIG_FILE_URL: {e}")
+    if not CONFIG_FILE_URL:
+        raise ValueError("CONFIG_FILE_URL is missing or empty")
+
+    res = rget(CONFIG_FILE_URL)
+    if res.status_code == 200:
+        with open('info.py', 'wb+') as f:
+            f.write(res.content)
+    else:
+        LOGGER.error(f"Failed to download info.py {res.status_code}")
 except Exception as e:
-    LOGGER.error(e)
-    pass
+    LOGGER.error(f"Error downloading CONFIG_FILE_URL: {e}")
