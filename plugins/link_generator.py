47498bd706ac930a83c353b38cad4866998a2863
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2024-03-29 20:58:33 +0530

    new

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -37,13 +37,10 @@ async def batch(client: Client, message: Message):
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
     
-    file_name = msg.document.file_name if msg.document else ""
-    file_size = msg.document.file_size if msg.document else ""
-
     link = f"https://telegram.me/{client.username}?start={base64_string}" if FINAL_URL is None or 'FINAL_URL' not in globals() else f"https://{FINAL_URL}?start={base64_string}"
     
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://t.me/share/url?url={link}')]])
-    await reply_text.edit(f"<b>File Name: {file_name}\n\nFile Size: {file_size}\n\nHere is Link:\n</b><code>{link}</code>", reply_markup=reply_markup, disable_web_page_preview = True)
+    await reply_text.edit(f"<b>Here is Link :\n<code>{link}</code></b>", reply_markup=reply_markup, disable_web_page_preview = True)
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
@@ -65,4 +62,4 @@ async def batch(client: Client, message: Message):
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://telegram.me/{client.username}?start={base64_string}" if FINAL_URL is None or 'FINAL_URL' not in globals() else f"https://{FINAL_URL}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://t.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+    await channel_message.reply_text(f"<b>Here is Link :\n<code>{link}</code></b>", quote=True, reply_markup=reply_markup)
